ea91eddee120a86a10b5b1d94116e3caa6609d48
Jacobitosuperstar <jacobomateobedoya@icloud.com>
2019-06-08 04:23:49 -0500

    minor changes

--- qr.py
+++ qr.py
@@ -1,13 +1,11 @@
 import pyqrcode
 
-
 def generate_qr():
     link_to_post = 'https://drive.google.com/file/d/1rCXu2iKENDXPyzmvo-E8HU8rroOe1p5_/view?usp=sharing'
     url = pyqrcode.create(link_to_post)
     url.png('url.png', scale=8)
     #print("Printing QR code")
     #print(url.terminal())
-
 
 if __name__ == '__main__':
     generate_qr()
