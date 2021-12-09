bbf00eaa09407ea5420e00ab0cc9c3b8867d202d
Jacobo Mateo Bedoya Oquendo <jacobobedoya@gmail.com>
2021-12-09 15:32:42 -0500

    actualizando el código generador de códigos QR

--- qr.py
+++ qr.py
@@ -1,11 +1,27 @@
-import pyqrcode
+import qrcode
+from PIL import Image
+import qrcode.image.svg
 
-def generate_qr():
-    link_to_post = 'https://drive.google.com/file/d/1rCXu2iKENDXPyzmvo-E8HU8rroOe1p5_/view?usp=sharing'
-    url = pyqrcode.create(link_to_post)
-    url.png('url.png', scale=8)
-    #print("Printing QR code")
-    #print(url.terminal())
+def generate_qr(url:str) -> None:
+    """Function that takes and URL and returns a QR code with the content in
+    SVG format.
+    """
+    qr = qrcode.QRCode(
+        version=40,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+        image_factory=qrcode.image.svg.SvgImage,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    image = qr.make_image(
+        fill_color="black",
+        back_color="white"
+    )
+    # .convert("RGB")
+    image.save("./qr_code.svg")
 
 if __name__ == '__main__':
-    generate_qr()
+    link_to_post = 'https://drive.google.com/file/d/1rCXu2iKENDXPyzmvo-E8HU8rroOe1p5_/view?usp=sharing'
+    generate_qr(link_to_post)
